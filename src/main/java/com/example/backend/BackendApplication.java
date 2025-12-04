dcad895452fdab632016ce0a70bf6afa3c9480f8
Magura-Witkowski,Stanisław S.M. <s.magurawitkowski@student.fontys.nl>
2025-12-04 12:04:05 +0100

    test

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,7 +2,6 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Main entry point for the Spring Boot application
 
 @SpringBootApplication
 public class BackendApplication {
